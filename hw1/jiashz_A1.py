2409ee0dd5e2b946769667d0e16b2ea267a199a3
Jiashuo Zhang <jiashz@uw.edu>
2018-01-11 14:09:48 -0800

    hw1 draft completed - need more testing

---
+++ hw1/jiashz_A1.py
@@ -0,0 +1,43 @@
+from random import *
+import math
+
+def run_Guess_My_Number():
+    n = randint(0, 1000)
+    print(n)
+    t = 0
+    score = 0
+    print("I have a secret number n.")
+    print("You can Ask, Guess, or Quit!")
+
+    while True:
+        option = input("Ask, Guess, or Quit? ")
+        t = t + 1
+        if option == "Quit":
+            print("Score: 0")
+            break
+        elif option == "Ask":
+            print("If we subtract k from n, is the result divisible by m?")
+            k = int(input("k? "))
+            m = int(input("m? "))
+            print(is_n_minus_k_divisible_by_m(n, k, m))
+        elif option == "Guess":
+            gString = input("What's your guess? ")
+            g = int(gString)
+            if g == n:
+                print("Score: " + str(math.ceil(n / t)))
+                break
+
+def isPrimeUnder1000(m):
+    if m >= 1000 or m <= 1: return False
+    for x in range(2, m):
+        if m % x == 0:
+            return False
+    return True
+
+def is_n_minus_k_divisible_by_m(n, k, m):
+    if not isPrimeUnder1000(m):
+        return False
+    return (n - k) % m == 0
+
+if __name__ == '__main__':
+    run_Guess_My_Number()
